0e2a581b8986656ab07dfa4f10d50cd0353ea787
tor <tor.erlend95@gmail.com>
2021-02-22 19:36:40 +0100

    MixtureKernel now works with vector of RNGs

--- src/kernels/mixture.jl
+++ src/kernels/mixture.jl
@@ -13,8 +13,27 @@ function AdvancedHMC.transition(
     z::PhasePoint
 )
     if rand(rng) < mix.γ
-        return transition(rng, h, mix.τ1, z)
+        return AdvancedHMC.transition(rng, h, mix.τ1, z)
     else
-        return transition(rng, h, mix.τ2, z)
+        return AdvancedHMC.transition(rng, h, mix.τ2, z)
     end
 end
+
+function AdvancedHMC.transition(
+    rng::AbstractVector{<:AbstractRNG},
+    h::Hamiltonian,
+    mix::MixtureKernel,
+    z::PhasePoint
+)
+    # TODO: is this always the correct thing to do?
+    # Ideally we'd allow different elements in the "batch"/vectorization
+    # use different components, BUT this will be faster and the resulting
+    # chains should still be valid. Similar to:
+    # https://github.com/TuringLang/AdvancedHMC.jl/pull/166#pullrequestreview-367216534
+    if AdvancedHMC.rand_coupled(rng) < mix.γ
+        return AdvancedHMC.transition(rng, h, mix.τ1, z)
+    else
+        return AdvancedHMC.transition(rng, h, mix.τ2, z)
+    end
+end
+
